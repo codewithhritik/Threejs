31e6e0a81795e5c80d2b5a397d7fbdeabe821dc1
codewithhritik <hritikbagane@gmail.com>
2020-06-15 00:46:14 +0530

    First Commit

---
+++ BasicsofThree.js/main.js
@@ -0,0 +1,79 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000)
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#e5e5e5");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//For Responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth/window.innerHeight;
+
+    camera.updateProjectionMatrix();
+});
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+//Sphere
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshLambertMaterial({ color: 0xf7f7f7 });
+// var mesh = new THREE.Mesh(geometry, material);
+
+// scene.add(mesh);
+
+meshX = -10;
+for(var i=0; i<15; i++){
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 10;
+    mesh.position.y = (Math.random() - 0.5) * 10;
+    mesh.position.z = (Math.random() - 0.5) * 10;
+    scene.add(mesh);
+    meshX+=1;
+}
+
+var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+light.position.set(0,0,0);
+scene.add(light);
+
+var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
+light.position.set(0,0,25);
+scene.add(light);
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    renderer.render(scene, camera);
+}
+
+function onMouseMove() {
+    event.preventDefault();
+
+    // Getting Mouse Position
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children, true);
+
+    for(var i=0; i<intersects.length; i++){
+        //intersects[i].object.material.color.set(0xff0000);
+        this.tl = new TimelineMax();
+        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.rotation, .5, {y: .5, ease: Expo.easeOut}, "=-1.5")
+    }
+}
+
+render();
+
+
+
+document.body.addEventListener('mousemove', onMouseMove);
